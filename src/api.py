c957900994c8cd638080568923e178be52f4c173
jwiemold <jwiemold@iastate.edu>
2021-03-15 18:01:07 -0500

    first refactor

--- src/api.py
+++ src/api.py
@@ -1,30 +1,10 @@
 import intake
 import numpy as np
-import satsearch
+from datetime import date
+import logging
+from satsearch import Search
 from rasterio.features import bounds as featurebounds
 from rio_tiler.io import COGReader
-
-STAC_API_ENDPOINT = "https://earth-search.aws.element84.com/v0/search/"
-
-
-def fetch_stac_scenes(geojson):
-    # date format:
-    dates = "2019-07-20/2019-08-05"
-    # dates = start_date + '/' + end_date
-    # TODO: Catch error if bad request passed to event
-    bounds = list(featurebounds(geojson))
-
-    results = satsearch.Search.search(
-        url=STAC_API_ENDPOINT,
-        collections=["sentinel-s2-l2a-cogs"],
-        datetime=dates,
-        bbox=bounds,
-        sort=["<datetime"],
-    )
-
-    scenes = intake.open_stac_item_collection(results.items())
-
-    return scenes
 
 
 def least_cloud_cover_date(scenes, geojson):
--- src/main.py
+++ src/main.py
@@ -3,11 +3,104 @@ import math
 from urllib.parse import urlparse
 
 import numpy as np
+import geopandas as gpd 
 from rasterio.features import bounds as featurebounds
-from rio_tiler.io import STACReader
+from rio_tiler.io import STACReader, COGReader
 from rio_tiler.models import ImageData
+from datetime import date
+from satsearch import Search
+from satstac import Item
 
 from src.api import fetch_stac_scenes, least_cloud_cover_date
+
+ELEMENT84_URL = "https://earth-search.aws.element84.com/v0/search/"
+SENTINEL_L2A_COLLECTION = 'sentinel-s2-l2a-cogs'
+CDL_PATH = "CDL_2019/CDL_2019_clip_20201217154034_1060751160.tif"
+
+
+def main(shapefile, datetime: date) -> object:
+    county_shapefile = gpd.read_file(shapefile)
+    county_shapefile = county_shapefile.to_crs({'init': 'epsg:4326'})
+    
+
+def search(geometry: dict, start: str, end: str) -> object:
+    kwargs = {
+        'bbox': geometry,
+        'datetime': f"{start}/{end}",
+        'collections': [SENTINEL_L2A_COLLECTION]
+    }
+    search = Search(url=ELEMENT84_URL, **kwargs)
+    return search.items()
+    
+
+def ndvi(geometry: tuple, stac_item: Item, height: float, width: float) -> ImageData: 
+    stac_item = "https://earth-search.aws.element84.com/v0/collections/sentinel-s2-l2a-cogs/items/{sceneid}"
+    stac_asset = stac_item.format(sceneid=stac_item.id) 
+        
+    with STACReader(stac_asset) as stac:
+        ndvi, _ = stac.part(geometry, expression="(B08-B04)/(B08+B04)")
+        
+    cloud_mask = cloud_mask()
+    ndvi_masked = ImageData(ndvi, cloud_mask).as_masked()
+    
+    return ndvi_masked
+        
+
+def cloud_mask(asset_url: str, geometry: tuple, height: float, width: float):
+    # TODO: STACReader or COGReader? 
+    with STACReader(asset_url) as stac:
+        scl_band, _ = stac.part(
+            geometry,
+            resampling_method="nearest",
+            height=height,
+            width=width,
+            assets="SCL",
+            max_size=None
+        )
+    # 0 - 4 are shadows, while pixels greater than 6 are cloud probable. 
+    cloud_mask = np.ma.where((scl_band < 4) | (scl_band > 6), 0, 1)
+    return cloud_mask
+        
+        
+def process_images(geometry: tuple, start: date, end: date) -> object:
+    try:
+        item = search(geometry, start, end)
+        
+        red, nir, grn = item.asset('B4'), item.asset('B5'), item.asset('B3')
+        red, nir, grn = red['href'], nir['href'], grn['href']
+        
+        with COGReader(red) as r:
+            red_band = r.part(geometry)
+            
+        with COGReader(nir) as n:
+            nir_band = n.part(geometry)
+            
+        with COGReader(grn) as g: 
+            green_band = g.part(geometry)
+            
+    except StopIteration:
+        raise Exception("No collection found for date range.")
+        
+
+def cdl_mask(geometry: tuple, crop_type: int):
+    with COGReader() as cog:
+        img = cog.part(geometry)
+        return np.ma.where((img.data == crop_type), 1, 0)
+
+
+def county_geometries(counties_df: GeoDataFrame) -> dict:
+    """ Takes a DataFrame and returns a Dict with the converted tuple coordinates. 
+
+    Args:
+        counties_df (GeoDataFrame): Counties GeoDataFrame from shapefile.
+
+    Returns:
+        dict: County names and tuple of geometries: {"County": "(36.5, ...) }.
+    """
+    counties = dict(zip(counties_df.COUNTY_NAM, counties_df.geometry))
+    for key, value in counties.items():
+        counties[key] = featurebounds(value)
+    return counties
 
 
 def yield_estimation(geojson):
